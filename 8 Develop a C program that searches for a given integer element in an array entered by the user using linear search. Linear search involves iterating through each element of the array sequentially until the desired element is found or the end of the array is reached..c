4d1c2217663e70e9b8fd538df83b6c1d511dd913
Rayhan@06 <gameover20643@gmail.com>
2024-06-03 20:52:13 +0600

    Create 8 Develop a C program that searches for a given integer element in an array entered by the user using linear search. Linear search involves iterating through each element of the array sequentially until the desired element is found or the end of the array is reached..c

---
+++ 8 Develop a C program that searches for a given integer element in an array entered by the user using linear search. Linear search involves iterating through each element of the array sequentially until the desired element is found or the end of the array is reached..c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int i,num[10];
+
+
+    printf("Enter the elements for array : ");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&num[i]);
+    }
+
+    int value;
+    int position = 0;
+
+    printf("Enter the value you want to search : ");
+    scanf("%d",&value);
+
+
+
+
+    for(i=0;i<10;i++)
+    {
+        if(value==num[i])
+        {
+            position = i+1;
+            break;
+        }
+    }
+    if(position==0)
+        printf("Not found");
+    else
+        printf("\nFound the value\nand\nThe position of %d is : %d\n",value,position);
+
+        getch();
+
+}
